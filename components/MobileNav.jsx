673d7e8fba15bfb22795a032397b13a1a32db40f
Marygraceee <ma.grasso1999@gmail.com>
2023-03-30 21:18:02 +0200

    il menu mobile va bene cosi

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -71,7 +71,7 @@ const MobileNav = () => {
                 exit="closed"
                 variants={menuVariants}
                 transition={menuTransition}
-                className="fixed top-0 left-0 w-full h-full bg-white z-50"
+                className="fixed top-0 left-0 w-full max-w-md h-screen bg-white z-50 overflow-y-auto"
               >
                 <button
                   onClick={() => setMenuOpen(false)}
